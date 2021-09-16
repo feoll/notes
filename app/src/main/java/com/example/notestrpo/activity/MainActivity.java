da9739b20293277df5b7c12f1ac84b45a7aeb107
feoll <alexandr.bechko@gmail.com>
2021-09-16 20:57:18 +0300

    init app

---
+++ app/src/main/java/com/example/notestrpo/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.notestrpo.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.notestrpo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
